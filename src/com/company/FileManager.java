68f8cab4e9483e89df80025d79d171fd635c3557
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-02-04 12:01:28 +0100

    idGeneratord method

--- src/com/company/FileManager.java
+++ src/com/company/FileManager.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.Random;
+
 public class FileManager {
 
     public FileManager() {
+    }
+
+    public String idGenerator() {
+        Random random = new Random();
+        int number = random.nextInt(99999999);
+        return String.format("%08d", number);
     }
 }
