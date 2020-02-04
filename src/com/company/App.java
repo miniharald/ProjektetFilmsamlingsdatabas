6a252c8e1b34ecd8a6ae50d702c3f156b696197a
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-02-04 18:54:00 +0100

    Created app class

---
+++ src/com/company/App.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    private List<AcademyAward> awards = new ArrayList<>();
+    private List<Movie> movies = new ArrayList<>();
+    private List<Director> directors = new ArrayList<>();
+    private List<Actor> actors = new ArrayList<>();
+    private List<Genre> genres = new ArrayList<>();
+    private List<Format> formats = new ArrayList<>();
+    private FileManager fileManager = new FileManager();
+
+    public App() {
+    }
+
+    public List<AcademyAward> getAwards() {
+        return awards;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public List<Director> getDirectors() {
+        return directors;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public List<Format> getFormats() {
+        return formats;
+    }
+}
