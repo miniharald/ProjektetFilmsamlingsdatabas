8d4e33c1588115673ff2001245dbb27e4ec0dfb2
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-02-18 00:12:13 +0100

    editera Oscars metoder skapade

--- src/com/company/methods/update/MovieObjectsUpdater.java
+++ src/com/company/methods/update/MovieObjectsUpdater.java
@@ -3,10 +3,8 @@ package com.company.methods.update;
 import com.company.App;
 import com.company.dbmaker.BaseObject;
 import com.company.dbmaker.FileManager;
-import com.company.objects.Actor;
-import com.company.objects.Director;
-import com.company.objects.Genre;
-import com.company.objects.Movie;
+import com.company.objects.*;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -33,12 +31,13 @@ public class MovieObjectsUpdater {
             app.getActors().get(choice).setLastName(lastName);
             fileManager.writeToFile(App.ACTORFOLDER + app.getActors().get(choice).getId() + ".txt", app.getActors().get(choice));
             for (Movie movie : app.getMovies()) {
-                for (Actor actor : movie.getCast())
+                for (Actor actor : movie.getCast()) {
                     if (actor.getId().equals(app.getActors().get(choice).getId())) {
                         movie.removeFromCast(actor);
                         movie.addToCast(app.getActors().get(choice));
                         fileManager.writeToFile(App.MOVIEFOLDER + movie.getId() + ".txt", movie);
                     }
+                }
             }
         }
     }
@@ -51,12 +50,13 @@ public class MovieObjectsUpdater {
             app.getDirectors().get(choice).setLastName(lastName);
             fileManager.writeToFile(App.DIRECTORFOLDER + app.getDirectors().get(choice).getId() + ".txt", app.getDirectors().get(choice));
             for (Movie movie : app.getMovies()) {
-                for (Director director : movie.getDirector())
+                for (Director director : movie.getDirector()) {
                     if (director.getId().equals(app.getDirectors().get(choice).getId())) {
                         movie.removeFromDirector(director);
                         movie.addToDirector(app.getDirectors().get(choice));
                         fileManager.writeToFile(App.MOVIEFOLDER + movie.getId() + ".txt", movie);
                     }
+                }
             }
         }
     }
@@ -68,13 +68,26 @@ public class MovieObjectsUpdater {
             app.getGenres().get(choice).setName(name);
             fileManager.writeToFile(App.GENREFOLDER + app.getGenres().get(choice).getId() + ".txt", app.getGenres().get(choice));
             for (Movie movie : app.getMovies()) {
-                for (Genre genre : movie.getGenre())
+                for (Genre genre : movie.getGenre()) {
                     if (genre.getId().equals(app.getActors().get(choice).getId())) {
                         movie.removeFromGenre(genre);
                         movie.addToGenre(app.getGenres().get(choice));
                         fileManager.writeToFile(App.MOVIEFOLDER + movie.getId() + ".txt", movie);
                     }
+                }
             }
+        }
+    }
+
+    public void updateAward(Object o) {
+        chooseObject(Collections.unmodifiableList(app.getAwards()));
+        if (choice < counter) {
+            inputNameOfObject();
+            app.getAwards().get(choice).setName(name);
+            fileManager.writeToFile(App.AWARDFOLDER + app.getAwards().get(choice).getId() + ".txt", app.getAwards().get(choice));
+            updateAwardInMovie();
+            updateAwardInActor();
+            updateAwardInDirector();
         }
     }
 
@@ -109,5 +122,41 @@ public class MovieObjectsUpdater {
     public void inputNameOfObject() {
         System.out.println("Namn:");
         name = scan.nextLine();
+    }
+
+    private void updateAwardInMovie() {
+        for (Movie movie : app.getMovies()) {
+            for (AcademyAward award : movie.getAwards()) {
+                if (award.getId().equals(app.getAwards().get(choice).getId())) {
+                    movie.removeFromAwards(award);
+                    movie.addToAwards(app.getAwards().get(choice));
+                    fileManager.writeToFile(App.MOVIEFOLDER + movie.getId() + ".txt", movie);
+                }
+            }
+        }
+    }
+
+    private void updateAwardInActor() {
+        for (Actor actor : app.getActors()) {
+            for (AcademyAward award : actor.getAwards()) {
+                if (award.getId().equals(app.getAwards().get(choice).getId())) {
+                    actor.removeFromAwards(award);
+                    actor.addToAwards(app.getAwards().get(choice));
+                    fileManager.writeToFile(App.ACTORFOLDER + actor.getId() + ".txt", actor);
+                }
+            }
+        }
+    }
+
+    private void updateAwardInDirector() {
+        for (Director director : app.getDirectors()) {
+            for (AcademyAward award : director.getAwards()) {
+                if (award.getId().equals(app.getAwards().get(choice).getId())) {
+                    director.removeFromAwards(award);
+                    director.addToAwards(app.getAwards().get(choice));
+                    fileManager.writeToFile(App.DIRECTORFOLDER + director.getId() + ".txt", director);
+                }
+            }
+        }
     }
 }
